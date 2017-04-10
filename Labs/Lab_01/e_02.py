4dc01a4889ecaa20c52b3dc8019c5b1591ca345f
Andre Fonseca <afbfonseca@gmail.com>
2017-04-10 10:00:40 +0100

    Linted e02 i03

--- Labs/Lab_01/e_02.py
+++ Labs/Lab_01/e_02.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    # item_01()
-    # item_02()
+    item_01()
+    item_02()
     item_03()
 
 
@@ -62,39 +62,30 @@ def item_02():
     plt.title('Lab1-2.2' + '\n' + r'$x(t)=cos(2 \pi(15)t) (u(t+2)-u(t+1.53))$')
     plt.grid()
 
+    plt.savefig('lab1-2.2.png', bbox_inches='tight', transparent=False)
+    plt.show()
+
 
 def item_03():
-     interval=[-5, 1]
-     t=np.linspace(interval[0], interval[1]+1, 1e4)
-     x=np.zeros(t.shape)
-     u1=(t >= 0) 
-     u2=(t + 4 >= 0) 
-     
-     
+    interval = [-5, 1]
+    t = np.linspace(interval[0], interval[1]+1, 1e4)
+    x = np.zeros(t.shape)
+    u1 = (t >= 0)
+    u2 = (t + 4 >= 0)
 
-     x=np.power(np.cos(((3.*np.pi*t)/4)-(np.pi/2.)),3)*(u1+(u2*(-1)))
-     
-     
-     plt.close('all')
-     plt.figure(facecolor='w')
-     plt.plot(t, x, 'k', linewidth=1.5)
-     
-     plt.xticks(fontsize=12)
-     plt.yticks(fontsize=12)
+    x = np.power(np.cos(((3 * np.pi * t) / 4) - (np.pi / 2)), 3) * (u1 + u2 * -1)
 
-     plt.xlabel('t(s)',fontsize=12)
-     plt.ylabel('x(t)',fontsize=12)
-     
-     plt.title('Lab1-2.3'+'\n'+r'$x(t)=\left ( cos \left ( \frac{3}{4} \pi t \right ) - \frac{\pi}{2} \right )^{3}(u(t)-u(t+4))$')
-     plt.grid()
+    plt.close('all')
+    plt.figure(facecolor='w')
+    plt.plot(t, x, 'k', linewidth=1.5)
 
+    plt.axis([interval[0], interval[1], -1, 1])
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
-
     plt.xlabel('t(s)', fontsize=12)
     plt.ylabel('x(t)', fontsize=12)
 
-    plt.title('Lab1 -2.3' + '\n' + r'$x(t)=\left ( cos \left ( \frac{3}{4} \pi t \right ) - \frac{\pi}{2} \right )^{3}(u(t)-u(t+4))$')
+    plt.title('Lab1-2.3'+'\n'+r'$x(t)=\left ( cos \left ( \frac{3}{4} \pi t \right ) - \frac{\pi}{2} \right )^{3}(u(t)-u(t+4))$')
     plt.grid()
 
     plt.savefig("lab1_2.3.png", bbox_inches='tight', transparent=False)
