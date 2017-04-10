23f2f0fb89416c8abe3a02ec20b3b7b60b680006
Andre Fonseca <afbfonseca@gmail.com>
2017-04-10 12:11:04 +0100

    Linted e03 i01

--- Labs/Lab_01/e_03.py
+++ Labs/Lab_01/e_03.py
@@ -2,13 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# TODO: Add graph titles, latex formulas, axis subtitles, axis best intervals
-
-# TODO: Draw 3 subplots for the following:
-# n = 1
-# n = 10
-# n = 10000
-
 
 def main():
     item_01()
@@ -39,51 +32,41 @@ def item_01():
     n = 10000
     x_3 = item_01_sum(n, f0, t)
 
-    # Subplot of 3 lines and 1 column
-#    figure, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    plt.figure( figsize=(8,8))
-    
-    
-    plt.subplot(3,1,1)
-    plt.plot(t,x_1)
-    
-    plt.axis([interval[0],interval[1],-1.5,1.5])
-    
+    plt.figure(facecolor='w', figsize=(8, 8))
+
+    # Subplt of 3 lines, 1 column, go to figure 1
+    plt.subplot(3, 1, 1)
+    plt.plot(t, x_1)
+
+    plt.axis([interval[0], interval[1], -1.5, 1.5])
     plt.xticks(np.arange(interval[0], interval[1]+1), fontsize=12)
     plt.yticks(np.arange(-1.5, 1.6, 0.5), fontsize=12)
-    
-    plt.title('Lab1-3.1' + '\n' + r'$x(t)=cos(2 \pi(15)t) (u(t+2)-u(t+1.53))$'+'\n'+'N=10')
-    
+
+    plt.title('Lab1-3.1' + '\n' + r'$x(t)=\frac{4}{ \pi} \sum_{k=1}^{N}\frac{sin(2 \pi (2k-1) f_{0}t)}{2k-1}$' + '\n' + 'N = 10')
     plt.grid()
 
-    
+    plt.subplot(3, 1, 2)
+    plt.plot(t, x_2)
 
-    plt.subplot(3,1,2)
-    plt.plot(t,x_2)
-    
-
-    plt.axis([interval[0],interval[1],-1.5,1.5])
+    plt.axis([interval[0], interval[1], -1.5, 1.5])
     plt.xticks(np.arange(interval[0], interval[1] + 1), fontsize=12)
     plt.yticks(np.arange(-1.5, 1.6, 0.5), fontsize=12)
-    
-    plt.title('N=10')
-    plt.grid()
-    
 
-    plt.subplot(3,1,3)
-    plt.plot(t,x_3)
-    
-    plt.axis([interval[0],interval[1],-1.5,1.5])
-    plt.xticks(np.arange(interval[0], interval[1] + 1), fontsize=12)
-    plt.yticks(np.arange(-1.5, 1.5,0.5), fontsize=12)
-    
-    plt.title('N=10000')
+    plt.title('N = 10')
     plt.grid()
-    
+
+    plt.subplot(3, 1, 3)
+    plt.plot(t, x_3)
+
+    plt.axis([interval[0], interval[1], -1.5, 1.5])
+    plt.xticks(np.arange(interval[0], interval[1] + 1), fontsize=12)
+    plt.yticks(np.arange(-1.5, 1.5, 0.5), fontsize=12)
+
+    plt.title('N = 10000')
+    plt.grid()
+
     plt.tight_layout()
-    
     plt.savefig('lab1-3.1.png', bbox_inches='tight', transparent=False)
-   
     plt.show()
 
 
