880a162a9194208d424a3094274a660ba808ab32
DanielCSantos <danielcsant4@gmail.com>
2017-04-12 01:50:49 +0100

    all ex 4 items main solution done
    Missing comments,labels and latex code

--- Labs/Lab_01/e_04.py
+++ Labs/Lab_01/e_04.py
@@ -10,6 +10,7 @@ def main():
     item_03()
     item_04()
     item_05()
+    item_06()
 
 
 def item_01():
@@ -28,11 +29,11 @@ def item_01():
     
     plt.grid()
     
-    plt.show()
+   
     
     
     plt.savefig("lab1_4.1.png", bbox_inches='tight', transparent=False)
-    
+    plt.show()
     plt.close('all')
 
 
@@ -49,11 +50,11 @@ def item_02():
     plt.axis([interval[0],interval[1],-120,150])
     
     plt.grid()
-    plt.show()
+    
     
     
     plt.savefig("lab1_4.2.png", bbox_inches='tight', transparent=False)
-    
+    plt.show()
     plt.close('all')
 
 
@@ -70,11 +71,11 @@ def item_03():
     plt.axis([interval[0],interval[1],-0.9,1.3])
     
     plt.grid()
-    plt.show()
+    
     
     
     plt.savefig("lab1_4.3.png", bbox_inches='tight', transparent=False)
-    
+    plt.show()
     plt.close('all')
 
 def item_04():
@@ -90,11 +91,11 @@ def item_04():
     plt.axis([interval[0],interval[1],-0.8,0.8])
     
     plt.grid()
-    plt.show()
+    
     
     
     plt.savefig("lab1_4.4.png", bbox_inches='tight', transparent=False)
-    
+    plt.show()
     plt.close('all')
 
 
@@ -108,13 +109,14 @@ def item_05():
     plt.figure(facecolor='w',figsize=(7,3.5))
     
     plt.stem(n,x,'k',linewidth=1.5)
-    plt.axis([interval[0],interval[1],-4,4])
+    plt.axis([interval[0],interval[1],-1.5,1.5  ])
     
     plt.grid()
+    plt.savefig("lab1_4.5.png", bbox_inches='tight', transparent=False)
     plt.show()
     
     
-    plt.savefig("lab1_4.5.png", bbox_inches='tight', transparent=False)
+   
     
     plt.close('all')
 
@@ -122,7 +124,25 @@ def item_05():
 
 
 def item_06():
-    pass
+    interval=[-40,40]
+    
+    n=np.arange(interval[0],interval[1]+1,1)
+    x=np.cos((2*np.pi/20.)*(50+10*np.cos(2*np.pi*n/20)))
+    
+    
+    plt.figure(facecolor='w',figsize=(7,3.5))
+    
+    plt.stem(n,x,'k',linewidth=1.5)
+    plt.axis([interval[0],interval[1],-1.5,1.5  ])
+    
+    plt.grid()
+    plt.savefig("lab1_4.6.png", bbox_inches='tight', transparent=False)
+    plt.show()
+    
+    
+   
+    
+    plt.close('all')
 
 
 if __name__ == '__main__':
