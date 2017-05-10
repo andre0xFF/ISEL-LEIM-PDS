c0dddbb46f5c824066d762c5f0246138aa0fa04c
Andre Fonseca <afbfonseca@gmail.com>
2017-05-10 10:16:50 +0100

    Today's class code

--- Labs/Lab_02/README.md
+++ Labs/Lab_02/README.md
@@ -1,7 +1,32 @@
 * Python module import outside package: https://askubuntu.com/questions/470982/how-to-add-a-python-module-to-syspath
 
 * Test script:
-```
+
+```shell
 python3.5 test.py
 playwave out.wav
+```
+
+* Notes about item 1
+
+f: average of student numbers
+A: group number
+
+* Mario song
+Try to incorporate this song in item 02
+
+```python
+import pysynth
+
+marioSong = (
+	('e5', 8), ('e5', 8), ('e5', 8), ('c5', 8), ('e5', 8),
+	('g5', 4), ('g4', 4),
+	('c5', 4), ('g4', 4), ('e4', 4),
+	('a4', 4), ('b4', 4), ('bb4', 8), ('a4', 4),
+	('g4', 4), ('e5', 4), ('g5', 4), ('a5', 4), ('f5', 8), ('g5', 8),
+	('e5', 8), ('c5', 8), ('d5', 8), ('b4', 8)
+)
+# test = (('c5', 8), ('c5', 8), ('c5', 8), ('g5', 8), ('e5', 4), ('d5', 8), ('c5', 4) )
+# pysynth.make_wav(test, fn = "test.wav")
+pysynth.make_wav(marioSong, bpm = 200, fn = "marioSong.wav")
 ```
--- Labs/Lab_02/e_02.py
+++ Labs/Lab_02/e_02.py
@@ -1,9 +1,67 @@
-import PySynth as pysynth
+import sys
+sys.path.append('../../PySynth')
+import pysynth_s as pysynth
+
+# E' necessario adaptar este script para PySynth
 
 
-def main():
-    pass
+def ADSR_env0(N=[]):
+    xEnv = np.ones(N)
+    return xEnv
 
 
-if __name__ == '__main__':
-    main()
+def ADSR_env1(N=[]):
+    Dur = (0.1, 0.2, 0.6, 0.1)
+    nA = np.floor(N * Dur[0])
+    nD = np.floor(N * Dur[1])
+    nS = np.floor(N * Dur[2])
+    nR = N - nA - nD - nS
+    zA = np.linspace(0, 1, nA)
+    # faltam aqui coisas
+    xD = np.linspace(1, .8, nD)
+    xS = np.ones(nS) * 0.8
+    xR = np.linspace(0.8, 0, nR)
+    # faltam aqui coisas
+    xEnv = np.hstack()
+    return xEnv
+
+
+def F2P3(noteList, notaDur):
+    intDur = 0.05
+    wavType = 3
+    Fs = 22050/2
+
+    x = np.array([])
+    for i in range(0, len(noteList)):
+        xtmp = Note_tone(noteList[i], notaDur[i], intDur, wavType, Fs)
+        x = np.hstack((x, xtmp))
+
+    return (x)
+
+
+def Note_tone(nota=[], notaDur=[], intDur=[], wavType=[], Fs=[]):
+
+    NotasDir = {}
+    strTmp = nota.upper()
+
+    print strTmp
+
+    fc = NotasDic[strTmp]
+    t = np.arange(0, notaDur, 1 / Fs)
+
+    if wavType == 1:
+        x = np.cos(2 * np.pi * fc * t)
+    elif wavType == 2:
+        x = sg.square(2 * np.pi * fc * t)
+    elif wavType == 3:
+        x = sg.sawtooth(2 * np.pi * fc * t, 0.5)
+    elif == 4:
+        x = sg.sawtooth(2 * np.pi * fc * t)
+    else:
+        print '\n wave type (%d) not available (only ints 1-4)\n'%wavType
+
+    xEnv = ADSR_env2(len(x))
+    plt.figure(facecolor='w')
+    plt.plot(xEnv)
+
+    # faltam aqui coisas
